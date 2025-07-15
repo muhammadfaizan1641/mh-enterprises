18c0317902f1c238060c60bfcdf027d3bc5612fd
muhammadfaizan1641 <mohdfaizaniit@gmail.com>
2025-07-15 18:09:44 +0530

    Navbar added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,10 @@
 import {Link} from "react-router-dom";
-import {useState} from "react";
 
 function Navbar(){
     
 
     return(
-        <div className="sm:flex bg-[#d4af37] gap-9 w-full sm:h-11 text-center pt-1  sm:justify-center pb-2" >
+        <div className="sm:flex bg-[#d4af37] gap-9 w-full sm:h-11 text-center pt-2  sm:justify-center pb-2" >
             <div>
                 <Link to='/' className='hover:text-white hover:border-white transition-all duration-300 border-b-2 border-black sm:border-none' >Home</Link>
             </div>
@@ -14,9 +13,6 @@ function Navbar(){
             </div>
             <div>
                 <Link to='/services' className='hover:text-white hover:border-white transition-all duration-300 border-b-2 border-black sm:border-none'>Services</Link>
-            </div>
-            <div>
-                <Link to='/portfolio' className='hover:text-white hover:border-white transition-all duration-300 border-b-2 border-black sm:border-none'>Portfolio</Link>
             </div>
             <div>
                 <Link to='/designs' className='hover:text-white hover:border-white transition-all duration-300 border-b-2 border-black sm:border-none'>Designs</Link>
